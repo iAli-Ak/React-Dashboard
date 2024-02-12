27786b547b62ab8e7ea9b9ccd1e5b512c35906db
Ali Khatam <mr.alkhatam@gmail.com>
2024-02-12 18:40:26 +0300

    added search component

---
+++ src/components/Search.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { CiSearch } from "react-icons/ci";
+
+const Search = () => {
+    return (
+        <div className='bg-half-transparent w-screen fixed top-0 right-0'>
+  <div className='bg-white dark:bg-[#484B52] w-1/2 h-full transform -translate-x-full transition duration-300 ease-in-out'>
+    <p>search</p>
+  </div>
+</div>
+    )
+}
+
+export default Search;
